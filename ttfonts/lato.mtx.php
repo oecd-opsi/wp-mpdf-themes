1b23338ac9da2f3ca07a1556fb0741a98b466759
conlaccento <antonio@blackstudio.it>
2020-10-14 11:05:27 +0200

    sync fonts

---
+++ ttfonts/lato.mtx.php
@@ -0,0 +1,38 @@
+<?php
+$name='Lato-Regular';
+$type='TTF';
+$desc=array (
+  'CapHeight' => 717.0,
+  'XHeight' => 507.0,
+  'FontBBox' => '[-94 -183 1117 919]',
+  'Flags' => 4,
+  'Ascent' => 919.0,
+  'Descent' => -183.0,
+  'Leading' => 0.0,
+  'ItalicAngle' => 0.0,
+  'StemV' => 87.0,
+  'MissingWidth' => 532.0,
+);
+$unitsPerEm=2000;
+$up=-70;
+$ut=60;
+$strp=250;
+$strs=60;
+$ttffile='/opt/bitnami/apps/wordpress/htdocs/wp-content/wp-mpdf-themes/ttfonts/Lato-Regular.ttf';
+$TTCfontID='0';
+$originalsize=75136;
+$sip=false;
+$smp=false;
+$BMPselected=false;
+$fontkey='lato';
+$panose=' 8 3 2 f 5 2 2 2 4 3 2 3';
+$haskerninfo=false;
+$haskernGPOS=false;
+$hassmallcapsGSUB=false;
+$fontmetrics='win';
+// TypoAscender/TypoDescender/TypoLineGap = 805, -195, 200
+// usWinAscent/usWinDescent = 987, -213
+// hhea Ascent/Descent/LineGap = 987, -213, 0
+$useOTL=0x0000;
+$rtlPUAstr='';
+?>
---
+++ ttfonts/latoB.mtx.php
@@ -0,0 +1,38 @@
+<?php
+$name='Lato-Bold';
+$type='TTF';
+$desc=array (
+  'CapHeight' => 723.0,
+  'XHeight' => 513.0,
+  'FontBBox' => '[-89 -181 1143 937]',
+  'Flags' => 262148,
+  'Ascent' => 937.0,
+  'Descent' => -181.0,
+  'Leading' => 0.0,
+  'ItalicAngle' => 0.0,
+  'StemV' => 165.0,
+  'MissingWidth' => 547.0,
+);
+$unitsPerEm=2000;
+$up=-60;
+$ut=80;
+$strp=250;
+$strs=80;
+$ttffile='/opt/bitnami/apps/wordpress/htdocs/wp-content/wp-mpdf-themes/ttfonts/Lato-Bold.ttf';
+$TTCfontID='0';
+$originalsize=73316;
+$sip=false;
+$smp=false;
+$BMPselected=false;
+$fontkey='latoB';
+$panose=' 8 3 2 f 8 2 2 2 4 3 2 3';
+$haskerninfo=false;
+$haskernGPOS=false;
+$hassmallcapsGSUB=false;
+$fontmetrics='win';
+// TypoAscender/TypoDescender/TypoLineGap = 805, -195, 200
+// usWinAscent/usWinDescent = 987, -213
+// hhea Ascent/Descent/LineGap = 987, -213, 0
+$useOTL=0x0000;
+$rtlPUAstr='';
+?>
